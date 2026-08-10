8548671cbcb63c0310d03cb8a541c25130006817
Mammekwa Calvin Malepe <75476253+mammekwa@users.noreply.github.com>
2026-08-10 10:16:14 +0200

    Initial Project Structure
    Spring project starter

---
+++ stokvelchain/src/main/java/za/co/tut/stokvelchain/StokvelchainApplication.java
@@ -0,0 +1,13 @@
+package za.co.tut.stokvelchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StokvelchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StokvelchainApplication.class, args);
+	}
+
+}
---
+++ stokvelchain/src/test/java/za/co/tut/stokvelchain/StokvelchainApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.tut.stokvelchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StokvelchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
